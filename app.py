18978c60682ceb48711aa3438494ece17f9f6855
hrishikeshsharma <sharmahrishikesh3@gmail.com>
2020-08-17 14:04:34 +0530

    this is commit

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-list = ['hrishi', 'harish', 'shashank', 'malayalam', 'mossom', 'sumanth']
-for x in list:
-    if x == x[::-1]:
-        print(x)
+def gcd(a, b):
+
+    if a == 0:
+        return b
+    if b == 0:
+        return a
+    if a == b:
+        return a
+    if a > b:
+        return gcd(a-b, a)
+    return gcd(a, b-a)
+
+print(gcd(36, 60))
